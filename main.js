579891284e975c5bd82c5eef77e7947e46e10c60
wfh0429 <872490934@qq.com>
2017-12-28 15:37:59 +0800

    2017.12.28
    爆兵

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-module.exports.loop = function(){
-    // Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1' );
-    var creep = Game.creeps['Harvester1'];
-    var source = creep.room.find(FIND_SOURCES);
-    if(creep.harvest(source[0]) == ERR_NOT_IN_RANGE){
-        creep.moveTo(source[0]);
+var roleHarvester = require('role.harvester');
+
+module.exports.loop = function () {
+    var index = 1;
+    if(Game.spawns['Spawn1'].energy >= 300 && Game.creeps.length < 5){ 
+        var name = 'worker' + index;
+        Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE , MOVE], name);
+        index = index + 1;
     }
-}
+    for(var name in Game.creeps) {
+        var creep = Game.creeps[name];
+        roleHarvester.run(creep);
+    }
+}  
